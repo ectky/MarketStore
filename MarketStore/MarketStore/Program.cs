5738cfc7174d7243016adb01fab506c9c52624c5
Elitsa <ectky3@gmail.com>
2020-07-08 14:32:53 +0300

    Add files via upload

---
+++ MarketStore/MarketStore/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketStore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var baseClient = new Client(Constants.baseClientName, Constants.baseClientAge);
+
+            var bronze = new BronzeCard(baseClient, 0);
+            PrintInfo(bronze, 150);
+
+            var silver = new SilverCard(baseClient, 600);
+            PrintInfo(silver, 850);
+
+            var gold = new GoldCard(baseClient, 1500);
+            PrintInfo(gold, 1300);
+        }
+
+        private static void PrintInfo(Card card, decimal purchaseValue)
+        {
+            Console.WriteLine($"Purchase value: ${purchaseValue:f2}");
+            Console.WriteLine($"Discount rate: {card.DiscountRate:f1}%");
+            Console.WriteLine($"Discount: ${card.CalculateDiscount(purchaseValue):f2}");
+            Console.WriteLine($"Total: ${card.CalculateTotal(purchaseValue):f2}");
+        }
+    }
+}
---
+++ MarketStore/MarketStore/SilverCard.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketStore
+{
+    internal class SilverCard : Card
+    {
+        public SilverCard(Client owner, decimal turnover) : base(owner, turnover)
+        {
+        }
+
+        public override decimal DiscountRate
+        {
+            get
+            {
+                if (this.Turnover >= 300)
+                {
+                    return this.discountRate = 3.5m;
+                }
+                else
+                {
+                    return this.discountRate = 2m;
+                }
+            }
+        }
+    }
+}
